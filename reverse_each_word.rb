23979719cf31935dbc378e0ba71b4d7912b7989e
Will Lee <will.i.lee@outlook.com>
2018-06-07 20:47:04 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,8 +2,8 @@ def reverse_each_word(string)
     temp_array = string.split()
     new_array= []
     new_array =
-    temp_array.collect do |word|
-      word.reverse
-    end
+      temp_array.collect do |word|
+        word.reverse
+      end
     new_array.join(" ")
 end
